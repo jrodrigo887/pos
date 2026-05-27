b0f5e501410767146e6a864d5b59297502ee84b1
JMarcospn <marcosace33@gmail.com>
2026-05-27 19:02:12 -0300

    criado o contatoRequest e contatoResponse na pasta dtos.

---
+++ src/main/java/com/agenda/dtos/ContatoRequest.java
@@ -0,0 +1,37 @@
+package com.agenda.dtos;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+
+public class ContatoRequest {
+    @NotBlank(message ="Nome obrigatótio")
+    private String nome;
+
+    @Email(message = "Email inválido")
+    private String email;
+
+     @NotBlank(message ="Telefone é obrigatório")
+    private String telefone;
+    
+//Getter Nome
+public String getNome() {
+return nome;
+}
+ //Setter nome
+ public void setNome(String nome) {
+this.nome = nome;
+ }
+ //Getter nome
+ public String getEmail() {
+    return email;
+ }
+  
+ //Setter Email
+ public void setEmail(String email){
+    this.email = email;
+ } 
+}
+
+
+
+
